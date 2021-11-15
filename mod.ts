906cf3504fbc9674f5e1b8e5726884c2580fe2c0
Bryan Grill <grillb@gmail.com>
2021-11-15 16:46:12 -0500

    add fetch class

--- mod.ts
+++ mod.ts
@@ -1,23 +1,27 @@
-import { decode, hmac } from "./deps.ts";
+import { decode, hmac } from './deps.ts';
 
-export interface CBAccessSignParams {
+export interface CBAccessSetup {
   apiKey: string;
   passPhrase: string;
   secret: string;
-  method: "GET" | "POST";
+}
+export interface CBAccessRequestParams {
+  method: 'GET' | 'POST';
   requestPath: string;
   body: string;
 }
 
-export type ContentType = "application/json";
+export type CBAccessOptionsParams = CBAccessSetup & CBAccessRequestParams;
+
+export type ContentType = 'application/json';
 
 export type CBAccessHeadersResp = {
   Accept: ContentType;
-  "Content-Type": ContentType;
-  "cb-access-key": string; // the API key
-  "cb-access-passphrase": string; // phasephrase when API registered
-  "cb-access-sign": string; // hmac sign
-  "cb-access-timestamp": number; // timestamp
+  'Content-Type': ContentType;
+  'cb-access-key': string; // the API key
+  'cb-access-passphrase': string; // phasephrase when API registered
+  'cb-access-sign': string; // hmac sign
+  'cb-access-timestamp': number; // timestamp
 };
 
 export type CbAccessFetchOptionsResp = {
@@ -33,25 +37,25 @@ export function cbAccessFetchOptions({
   method,
   requestPath,
   body,
-}: CBAccessSignParams): CbAccessFetchOptionsResp {
+}: CBAccessOptionsParams): CbAccessFetchOptionsResp {
   const timeStamp = Date.now() / 1000;
   const key = decode(secret);
   const msg = `${timeStamp}${method}${requestPath}${body}`;
-  const sign = hmac("sha256", key, msg, "utf8", "base64") as string;
+  const sign = hmac('sha256', key, msg, 'utf8', 'base64') as string;
 
   const headers = {
-    Accept: "application/json" as ContentType,
-    "Content-Type": "application/json" as ContentType,
-    "cb-access-key": apiKey,
-    "cb-access-passphrase": passPhrase,
-    "cb-access-sign": sign,
-    "cb-access-timestamp": timeStamp,
+    Accept: 'application/json' as ContentType,
+    'Content-Type': 'application/json' as ContentType,
+    'cb-access-key': apiKey,
+    'cb-access-passphrase': passPhrase,
+    'cb-access-sign': sign,
+    'cb-access-timestamp': timeStamp,
   };
 
   const options = {
     method,
     headers,
-    ...(method === "POST" && { body }),
+    ...(method === 'POST' && { body }),
   } as unknown as RequestInit;
 
   return {
@@ -60,5 +64,14 @@ export function cbAccessFetchOptions({
   };
 }
 
-// TOOO: init class with api secrets
-// add fetch function that returns data
+export class CBFetch {
+  setup: CBAccessSetup;
+
+  constructor(setup: CBAccessSetup) {
+    this.setup = setup;
+  }
+
+  options(requestOptions: CBAccessRequestParams) {
+    return cbAccessFetchOptions({ ...this.setup, ...requestOptions });
+  }
+}
