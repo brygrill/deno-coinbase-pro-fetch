bb5a146ad55d15d769da761170aa33ffd0e32ff1
Bryan Grill <grillb@gmail.com>
2021-11-15 16:48:33 -0500

    run fmt

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { decode, hmac } from './deps.ts';
+import { decode, hmac } from "./deps.ts";
 
 export interface CBAccessSetup {
   apiKey: string;
@@ -6,22 +6,22 @@ export interface CBAccessSetup {
   secret: string;
 }
 export interface CBAccessRequestParams {
-  method: 'GET' | 'POST';
+  method: "GET" | "POST";
   requestPath: string;
   body: string;
 }
 
 export type CBAccessOptionsParams = CBAccessSetup & CBAccessRequestParams;
 
-export type ContentType = 'application/json';
+export type ContentType = "application/json";
 
 export type CBAccessHeadersResp = {
   Accept: ContentType;
-  'Content-Type': ContentType;
-  'cb-access-key': string; // the API key
-  'cb-access-passphrase': string; // phasephrase when API registered
-  'cb-access-sign': string; // hmac sign
-  'cb-access-timestamp': number; // timestamp
+  "Content-Type": ContentType;
+  "cb-access-key": string; // the API key
+  "cb-access-passphrase": string; // phasephrase when API registered
+  "cb-access-sign": string; // hmac sign
+  "cb-access-timestamp": number; // timestamp
 };
 
 export type CbAccessFetchOptionsResp = {
@@ -41,21 +41,21 @@ export function cbAccessFetchOptions({
   const timeStamp = Date.now() / 1000;
   const key = decode(secret);
   const msg = `${timeStamp}${method}${requestPath}${body}`;
-  const sign = hmac('sha256', key, msg, 'utf8', 'base64') as string;
+  const sign = hmac("sha256", key, msg, "utf8", "base64") as string;
 
   const headers = {
-    Accept: 'application/json' as ContentType,
-    'Content-Type': 'application/json' as ContentType,
-    'cb-access-key': apiKey,
-    'cb-access-passphrase': passPhrase,
-    'cb-access-sign': sign,
-    'cb-access-timestamp': timeStamp,
+    Accept: "application/json" as ContentType,
+    "Content-Type": "application/json" as ContentType,
+    "cb-access-key": apiKey,
+    "cb-access-passphrase": passPhrase,
+    "cb-access-sign": sign,
+    "cb-access-timestamp": timeStamp,
   };
 
   const options = {
     method,
     headers,
-    ...(method === 'POST' && { body }),
+    ...(method === "POST" && { body }),
   } as unknown as RequestInit;
 
   return {
