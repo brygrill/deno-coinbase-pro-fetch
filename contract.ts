c8cf777a846f024c80b2bca2efe227bf1121afbc
Bryan Grill <grillb@gmail.com>
2021-11-18 16:57:44 -0500

    start setup api contract

---
+++ contract.ts
@@ -0,0 +1,17 @@
+/** Types for Coinbase API endpoints */
+
+export interface Account {
+  id: string;
+  currency: string;
+  balance: string;
+  available: string;
+  hold: string;
+  "profile_id": string;
+  "trading_enabled": boolean;
+}
+
+export interface AccountExtended extends Account{
+  balanceNum: number;
+  availableNum: number;
+  holdNum: number;
+}
