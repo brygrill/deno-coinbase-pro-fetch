57ae568f917456588a469e591e9d5f41517594fc
Bryan Grill <grillb@gmail.com>
2021-12-02 17:39:18 -0500

    debug...

--- fetch/endpoints_test.ts
+++ fetch/endpoints_test.ts
@@ -1,17 +1,17 @@
-// import { superdeno } from "https://deno.land/x/superdeno/mod.ts";
-// import { opine } from "https://deno.land/x/opine@1.9.1/mod.ts";
+import { superdeno } from "https://deno.land/x/superdeno/mod.ts";
+import { opine } from "https://deno.land/x/opine@1.9.1/mod.ts";
 
-// const app = opine();
+const app = opine();
 
-// app.get("/user", (_req, res) => {
-//   res.setStatus(200).json({ name: "Deno" });
-// });
+app.get("/user", (_req, res) => {
+  res.setStatus(200).json({ name: "Deno" });
+});
 
-// superdeno(app)
-//   .get("/user")
-//   .expect("Content-Type", /json/)
-//   .expect("Content-Length", "15")
-//   .expect(200)
-//   .end((err, _res) => {
-//     if (err) throw err;
-//   });
+superdeno(app)
+  .get("/user")
+  .expect("Content-Type", /json/)
+  .expect("Content-Length", "15")
+  .expect(200)
+  .end((err, _res) => {
+    if (err) throw err;
+  });
